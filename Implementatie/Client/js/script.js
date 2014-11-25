be2632672e3ddbba2e1f15e54072766b0fa2d348
Leroy Witteveen <info@leroydev.nl>
2014-11-25 11:10:39 +0100

    .idea files verwijderd en implementatie objecten tekenen

--- Implementatie/Client/js/script.js
+++ Implementatie/Client/js/script.js
@@ -1,6 +1,7 @@
-var app = angular.module("Percolator", ["ngRoute", "ngTouch", "mobile-angular-ui"]);
+var app = angular.module("Percolator", ["ngRoute", "ngTouch", "mobile-angular-ui"]),
+		canvas;
 
-app.controller("MainController", function($scope) {
+app.controller("toolbarController", function($scope) {
 	$scope.toolbarToggled = true;
 	$scope.changeToggle = function() {
 		if (!$scope.toolbarToggled) {
@@ -15,5 +16,46 @@ app.controller("MainController", function($scope) {
 		} else {
 			return false;
 		}
+	};
+
+	$scope.addObject = function() {
+		canvas.add(
+			new fabric.Rect({
+				width:100,
+				height:100,
+				top:50,
+				left:50,
+				fill: "white",
+				hasControls: false,
+				stroke: "black"
+			})
+		);
+	};
+});
+
+app.controller("canvasController", function($scope) {
+	$scope.initializeCanvas = function() {
+		canvas = new fabric.Canvas("geheugenModel");
+		canvas.on({
+			//Making moved / selected object transparent for a nice effect
+			'mouse:down': function(e) {
+				if (e.target) {
+					e.target.opacity = 0.5;
+					canvas.renderAll();
+				}
+			},
+			'mouse:up': function(e) {
+				if (e.target) {
+					e.target.opacity = 1;
+					canvas.renderAll();
+				}
+			},
+			'object:moved': function(e) {
+				e.target.opacity = 0.5;
+			},
+			'object:modified': function(e) {
+				e.target.opacity = 1;
+			}
+		});
 	};
 });
